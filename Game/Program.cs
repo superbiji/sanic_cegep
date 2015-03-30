e769bbbfd59350c77d322f858cdf85caeb1597a9
superbiji <benjamin.lapointe13@gmail.com>
2015-03-29 21:34:12 -0400

    1er commit

---
+++ Game/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace Game
+{
+	static class Program
+	{
+		static void Main()
+		{
+			RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Untitled");
+			window.SetFramerateLimit(60);
+			window.Closed += new EventHandler(OnClose);
+
+			while (window.IsOpen)
+			{
+				window.DispatchEvents();
+				window.Clear();
+				//window.Draw(Drawable);
+				window.Display();
+			}
+		}
+
+		static void OnClose(object sender, EventArgs e)
+		{
+			RenderWindow window = (RenderWindow)sender;
+			window.Close();
+		}
+	}
+}
