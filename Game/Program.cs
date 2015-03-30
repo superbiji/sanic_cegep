09c508a99f83e03240d522c14789a7d8dc9e6c50
superbiji <benjamin.lapointe13@gmail.com>
2015-03-30 11:57:34 -0400

    zcxccxzxczxzc

--- Game/Program.cs
+++ Game/Program.cs
@@ -16,18 +16,73 @@ namespace Game
 {
 	static class Program
 	{
+		static RenderWindow window;
+		static Sprite sanic;
+		static Vector2f sanic_sped = new Vector2f(0, 0);
+
 		static void Main()
 		{
-			RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Untitled");
+			window = new RenderWindow(new VideoMode(800, 600), "Untitled");
 			window.SetFramerateLimit(60);
 			window.Closed += new EventHandler(OnClose);
+			window.KeyPressed += new EventHandler<KeyEventArgs>(KeyPressed);
+
+			Vector2f GRAVITY = new Vector2f(0, 1);
+			sanic = new Sprite(new Texture(@"..\..\Ressources\sanic.png"));
+			sanic.Scale = new Vector2f(0.25f, 0.25f);
 
 			while (window.IsOpen)
 			{
+				if (!Keyboard.IsKeyPressed(Keyboard.Key.Left) && !(Keyboard.IsKeyPressed(Keyboard.Key.Right)))
+				{
+					sanic_sped.X /= 1.1f;
+				}
+				if (sanic.Position.X < 0)
+				{
+					//sanic.Position = new Vector2f(0, sanic.Position.Y);
+					sanic_sped.X = Math.Abs(sanic_sped.X);
+				}
+				else if (sanic.Position.X + sanic.GetGlobalBounds().Width > window.Size.X)
+				{
+					//sanic.Position = new Vector2f(window.Size.X - sanic.GetGlobalBounds().Width, sanic.Position.Y);
+					sanic_sped.X = -Math.Abs(sanic_sped.X); ;
+				}
+				if (sanic.Position.Y + sanic.GetGlobalBounds().Height < window.Size.Y)
+				{
+					sanic_sped += GRAVITY;
+				}
+				else
+				{
+					sanic_sped.Y = sanic_sped.Y < 0 ? sanic_sped.Y : 0;
+				}
+				sanic.Position += sanic_sped;
+
 				window.DispatchEvents();
 				window.Clear(Color.Yellow);
-				//window.Draw(Drawable);
+				window.Draw(sanic);
 				window.Display();
+			}
+		}
+
+		static void KeyPressed(object sender, KeyEventArgs e)
+		{
+			Vector2f VITESSE_X = new Vector2f(2, 0);
+			Vector2f VITESSE_Y = new Vector2f(0, -25);
+			
+			if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+			{
+				sanic_sped -= VITESSE_X;
+			}
+			if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+			{
+				sanic_sped += VITESSE_X;
+			}
+			if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+			{
+				if (sanic.Position.Y + sanic.GetGlobalBounds().Height > window.Size.Y)
+				{
+					sanic_sped += VITESSE_Y;
+				}
 			}
 		}
 
