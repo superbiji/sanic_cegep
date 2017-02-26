2e6749ccd99e3c4e25cc6420ae77537ae4ba807d
Sirmyself <jerome_masmor@hotmail.com>
2017-02-26 03:33:43 -0500

    Sanic & Squidnic are back

--- Game/Program.cs
+++ Game/Program.cs
@@ -12,63 +12,88 @@ using System.Threading.Tasks;
 
 namespace Game
 {
-	static class Program
-	{
-		static int Main()
+    static class Program
+    {
+        static int Main()
         {
-            int nbrSanic = 1;
-            
+            int nbrSanic = 0;
+
             int hautFen = 900;
             bool fullscreen = false;
             float ratioVoulu = 16f / 9f;
 
             intro();
 
-			RenderWindow window = new RenderWindow(new VideoMode((uint)(Math.Round(hautFen*ratioVoulu)), (uint)hautFen), "SANIC SPED!!", fullscreen ? Styles.Fullscreen : /**/Styles.None); //Changer "Styles.None" pour "Styles.Close" pour récupérer le bouton pour fermer la fenêtre
-            
-			window.Closed += new EventHandler(OnClose);
-			window.SetFramerateLimit(60);
-			window.SetKeyRepeatEnabled(false);
+            RenderWindow window = new RenderWindow(new VideoMode((uint)(Math.Round(hautFen * ratioVoulu)), (uint)hautFen), "SANIC SPED!!", fullscreen ? Styles.Fullscreen : /**/Styles.None); //Changer "Styles.None" pour "Styles.Close" pour récupérer le bouton pour fermer la fenêtre
 
-			Music teme = new Music(@"..\..\Ressources\SanicMusic.wav");
-			teme.Volume = 10;
-			teme.Loop = true;
-		   
-			Sprite background = new Sprite(new Texture(@"..\..\Ressources\Background.jpg"));
-			background.Scale = new Vector2f(window.Size.X / background.GetLocalBounds().Width, window.Size.Y / background.GetLocalBounds().Height);
+            window.Closed += new EventHandler(OnClose);
+            window.SetFramerateLimit(60);
+            window.SetKeyRepeatEnabled(false);
 
-            //Liste pour le loll MOUHAHAHAHAHA
-            List<Sanic> sanic = new List<Sanic>();
-            for (int ji = 0; ji < nbrSanic; ji++ )
+            Music teme = nbrSanic <= 0 ? new Music(@"..\..\Ressources\gloria.wav") : new Music(@"..\..\Ressources\SanicMusic.wav");
+            teme.Volume = 10;
+            teme.Loop = true;
+
+            Sprite background = new Sprite(new Texture(@"..\..\Ressources\Background.jpg"));
+            background.Scale = new Vector2f(window.Size.X / background.GetLocalBounds().Width, window.Size.Y / background.GetLocalBounds().Height);
+
+
+            if (nbrSanic > 0)
             {
-                sanic.Add(new Sanic(window));
-                float posX = ((window.Size.X - sanic[ji].Size.X) * (ji + 1) / (1 + nbrSanic));
-                sanic[ji].Position = new Vector2f(posX, 0);
+                teme.Volume = 10;
+                //Liste pour le loll MOUHAHAHAHAHA
+                List<Sanic> sanic = new List<Sanic>();
+                for (int ji = 0; ji < nbrSanic; ji++)
+                {
+                    sanic.Add(new Sanic(window));
+                    float posX = ((window.Size.X - sanic[ji].Size.X) * (ji + 1) / (1 + nbrSanic));
+                    sanic[ji].Position = new Vector2f(posX, 0);
+                }
+                teme.Play();
+                sanic[0].Quote(0);
+
+                while (window.IsOpen)
+                {
+                    window.DispatchEvents();
+
+                    window.Clear();
+                    window.Draw(background);
+                    foreach (Sanic s in sanic)
+                    {
+                        s.Update();
+                        window.Draw(s);
+                    }
+                    window.Display();
+                    if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                    {
+                        window.Close();
+                    }
+                }
             }
-			teme.Play();
+            else
+            {
+                teme.Volume = 90;
+                Squidnic s = new Squidnic(window);
+                teme.Play();
 
-
-            sanic[0].Quote(0);
-
-			while (window.IsOpen)
-			{
-				window.DispatchEvents();
-
-				window.Clear();
-				window.Draw(background);
-                foreach (Sanic s in sanic)
+                while (window.IsOpen)
                 {
-                    s.Update();
-				    window.Draw(s);
+                    window.DispatchEvents();
+
+                    window.Clear();
+                    window.Draw(background);
+                    s.update();
+                    window.Draw(s);
+                    window.Display();
+                    if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                    {
+                        window.Close();
+                    }
                 }
-				window.Display();
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
-                {
-                    window.Close();
-                }
-			}
+
+            }
             return 0;
-		}
+        }
 
         static void intro()
         {
@@ -98,10 +123,10 @@ namespace Game
             splashScreen.Close();
         }
 
-		static void OnClose(object sender, EventArgs e)
-		{
-			RenderWindow window = (RenderWindow)sender;
-			window.Close();
-		}
-	}
+        static void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+    }
 }
